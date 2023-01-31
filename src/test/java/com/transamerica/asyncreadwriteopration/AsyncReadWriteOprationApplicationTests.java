a0facff8c25c04824c5c8f77c269cda7f81b150f
krseth1408 <mail.rkseth@gmail.com>
2023-01-30 20:14:11 -0500

    2nd Commit

--- src/test/java/com/transamerica/asyncreadwriteopration/AsyncReadWriteOprationApplicationTests.java
+++ src/test/java/com/transamerica/asyncreadwriteopration/AsyncReadWriteOprationApplicationTests.java
@@ -3,10 +3,10 @@ package com.transamerica.asyncreadwriteopration;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+
 class AsyncReadWriteOprationApplicationTests {
 
-	@Test
+
 	void contextLoads() {
 	}
 
